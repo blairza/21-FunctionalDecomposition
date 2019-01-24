80ec0b3cc6408ed943f57b954d1b152a0f937adc
blairza <blairza@rose-hulman.edu>
2019-01-24 15:59:24 -0500

    DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,10 +1,13 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: Trey Kline and Zane Blair.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
 
 ####### Do NOT attempt this assignment before class! #######
 
+def main():
+    print('E')
+main()
